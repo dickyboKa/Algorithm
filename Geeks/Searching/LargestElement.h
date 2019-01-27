f71ba65c05f3daf6b934c966bb778ba6ec172d2e
Dicky Kamarullah <dicky.kamar14@gmail.com>
2019-01-27 12:28:42 +0700

    Find a 3 biggest element on Array with time complexitiy of O(n)

---
+++ Geeks/Searching/LargestElement.h
@@ -0,0 +1,30 @@
+#include <vector>
+
+
+/*
+answer to: https://www.geeksforgeeks.org/find-the-largest-three-elements-in-an-array/
+this should be: O(n)
+*/
+std::vector<int> findlargestElement(std::vector<int> &a)
+{
+	int first, second, third;
+	first = second = third = INT_MIN;
+
+	for (auto it = a.cbegin(); it != a.cend(); ++it)
+	{
+		if (*it > first)
+		{
+			third = second;
+			second = first;
+			first = *it;
+		}
+		else if (*it > second)
+		{
+			third = second;
+			second = *it;
+		}
+		else
+			third = *it;
+	}
+	return std::vector<int>{first, second, third};
+}
