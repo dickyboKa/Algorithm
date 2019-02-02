4976e26cb7fb94d71a12c6b4cf2e58760d46e664
Dicky Kamarullah <dicky.kamar14@gmail.com>
2019-02-02 09:47:08 +0700

    Combination Sums, but the result is consecutive numbers only

--- Geeks/Mathematical/Combinations.h
+++ Geeks/Mathematical/Combinations.h
@@ -34,9 +34,46 @@ void findCombinationsSum(std::vector<int> &arr, int index,
 	}
 }
 
-
 void findCombinationsSum(int n)
 {
 	std::vector<int> arr;
 	findCombinationsSum(arr, 0, n, n);
 }
+
+
+/*
+answer to geeks: https://www.geeksforgeeks.org/print-possible-sums-consecutive-numbers-sum-n/
+this would run on 0(n)
+this different than above sollutions, in term of combinations it made. The boundary of this problem, is on the need to found
+the numbers in consecutive manners (adjacent to one another with diff 1);
+*/
+void consecutiveNumbersSum(int N)
+{
+	int start, end, sum;
+	start = end = sum = 1;
+
+	while (start <= N / 2) // why n/2: because n/2 + (n/2 + 1), would always bigger than n;
+	{
+		if (sum < N)
+		{
+			end += 1;
+			sum += end;
+		}
+		else if (sum > N)
+		{
+			// found starting number that would fit in;
+			sum -= start;
+			start += 1;
+		}
+		else if (sum == N)
+		{
+			for (int i = start; i <= end; ++i)
+				std::cout << i << " ";
+			std::cout << std::endl;
+			sum -= start;
+			start += 1; // look for another combination
+		}
+	}
+
+}
+
