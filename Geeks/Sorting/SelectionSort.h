f647707b40ac16acdfa6a380dd55d97ce6f53d18
Dicky Kamarullah <dicky.kamar14@gmail.com>
2019-02-06 19:27:02 +0700

    Very Simple Selection Sort

---
+++ Geeks/Sorting/SelectionSort.h
@@ -0,0 +1,26 @@
+#include <vector>
+#include <iostream>
+
+/*
+Selection Sort: O(N^2);
+*/
+void selectionSort(std::vector<int> &arr)
+{
+	for(auto it = arr.begin(); it != arr.end(); ++it)
+	{
+		auto temp_it = it;
+		for (auto m_it = it + 1; m_it != arr.cend(); ++m_it)
+		{
+			if (*m_it < *temp_it)
+				temp_it = m_it;
+		}
+
+		//swap the smallest to front
+		if (temp_it != it)
+		{
+			int temp = *it;
+			*it = *temp_it;
+			*temp_it = temp;
+		}
+	}
+}
