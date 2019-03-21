70f8d3c2ec016ecf3060f9ab9508dfa9dcc0c917
Dicky Kamarullah <dicky.kamar14@gmail.com>
2019-03-22 06:49:06 +0700

    Modified Graph Class, and add Breadth First Search

--- Geeks/Graph/GraphList.h
+++ Geeks/Graph/GraphList.h
@@ -1,16 +1,20 @@
 #include <vector>
 #include <list>
 #include <iostream>
+#include <iterator>
 
 //using vector better cache
 class GraphList
 {
 public:
-	GraphList(int vertexSize) : graph(vertexSize) {};
+	GraphList(int v) : vertices(v), graph(v), directed(false){};
+	GraphList(int v, bool direct) : vertices(v), graph(v), directed(direct) {};
 
 	void addEdge(int u, int v)
 	{
 		graph[u].push_back(v);
+		if (directed)
+			return;
 		graph[v].push_back(u);
 	}
 
@@ -25,6 +29,49 @@ public:
 			std::cout << std::endl;
 		}
 	}
+
+	int getVertices() { return vertices; }
+	// iterable
+	auto cbegin(int vertex) { return graph[vertex].cbegin(); }
+
+	auto cend(int vertex) { return graph[vertex].cend(); }
+
 private:
+	int vertices;
+	bool directed;
 	std::vector<std::vector<int>> graph;
 };
+
+
+// return a path that exist
+std::vector<int> breadthFirstSearch(GraphList &graph, int vectorStart)
+{
+	std::vector<bool> visited(graph.getVertices());
+
+	std::list<int> queueToCheck;
+	// mark start to visited, because we about to visit start
+	visited[vectorStart] = true;
+	// and put start in queue to check for the path
+	queueToCheck.push_front(vectorStart);
+
+	std::vector<int> path;
+	while (!queueToCheck.empty()) //while there still vertex to check
+	{
+		// get vertex that finish traverse, and put it in path
+		int vertex = queueToCheck.front();
+		// actually we can do anything here for the vertex that already found, but for now just populate it into path array
+		path.push_back(vertex);
+		queueToCheck.pop_front(); 
+
+		for (auto it = graph.cbegin(vertex); it != graph.cend(vertex); ++it)
+		{
+			if (!visited[*it])
+			{
+				visited[*it] = true;
+				queueToCheck.push_back(*it);
+			}
+		}
+	}
+
+	return path;
+}
