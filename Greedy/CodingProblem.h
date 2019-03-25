e6fc648425f09f98d48be5b80ab0d89379411979
Dicky Kamarullah <dicky.kamar14@gmail.com>
2019-03-25 07:51:01 +0700

    Greedy Algorithm Activities Selection Problem

---
+++ Greedy/CodingProblem.h
@@ -0,0 +1,45 @@
+#include <vector>
+#include <algorithm>
+#include <iostream>
+
+// activities selection problem
+std::vector<std::pair<int, int>> activitiesPairsStartFinish(std::vector<int> &start, std::vector<int> &finish)
+{
+	std::vector<std::pair<int, int>> activites;
+
+	auto s = start.cbegin();
+	auto f = finish.cbegin();
+	while (s != start.cend())
+	{
+		activites.push_back(std::make_pair(*s, *f));
+		++s;
+		++f;
+	}
+	return activites;
+}
+
+void sortByFinishTime(std::vector<std::pair<int, int>> &activites)
+{
+	std::sort(activites.begin(), activites.end(), [](std::pair<int, int> a, std::pair<int, int> b) {
+		return a.second < b.second;
+	});
+}
+
+void printMaxActivites(std::vector<int> &startTime, std::vector<int> &finishTime)
+{
+	std::vector<std::pair<int, int>> activites = activitiesPairsStartFinish(startTime, finishTime);
+	sortByFinishTime(activites);
+		
+	auto print_it = activites.cbegin();
+	std::cout << print_it->first << " / " << print_it->second << std::endl;
+
+	// first = is start time, second = is finish time
+	for (auto it = activites.cbegin() + 1; it != activites.cend(); ++it)
+	{
+		if (it->first > print_it->second)
+		{
+			print_it = it;
+			std::cout << print_it->first << " / " << print_it->second << std::endl;
+		}
+	}
+}
