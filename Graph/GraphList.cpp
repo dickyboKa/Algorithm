5090e4ff747685e0411346fcf82a30935e9d6dc3
Dicky Kamarullah <dicky.kamar14@gmail.com>
2019-03-25 07:15:24 +0700

    refactoring GraphList, change to use Set for container better logn for search

---
+++ Graph/GraphList.cpp
@@ -0,0 +1,105 @@
+#include "GraphList.h"
+
+void GraphList::addEdge(int u, int v)
+{
+	graph[u].insert(v);
+	if (directed)
+		return;
+	graph[v].insert(u);
+}
+
+void GraphList::print()
+{
+	for (int v = 0; v < graph.size(); ++v)
+	{
+		std::cout << std::endl << "Adjacency list of vertex "
+			<< v << std::endl << "head ";
+		for (auto x : graph[v])
+			std::cout << "->" << x;
+		std::cout << std::endl;
+	}
+}
+
+void GraphList::searchEdge(int src, int dest)
+{
+	auto itr = graph[src].find(dest);
+	if (itr == graph[src].end())
+		std::cout << std::endl << "Edge from " << src
+		<< " to " << dest << " not found."
+		<< std::endl;
+	else
+		std::cout << std::endl << "Edge from " << src
+		<< " to " << dest << " found."
+		<< std::endl;
+}
+
+std::vector<int> GraphList::breadthFirstSearch(int vectorStart)
+{
+	std::vector<bool> visited(vertices);
+
+	std::list<int> queueVertexToGo;
+	visited[vectorStart] = true;
+
+	queueVertexToGo.push_front(vectorStart);
+
+	std::vector<int> path;
+	while (!queueVertexToGo.empty())
+	{
+		int currentlyOnVertex = queueVertexToGo.front();
+		// actually we can do anything here for the vertex that already found, but for now just populate it into path array
+		path.push_back(currentlyOnVertex);
+		queueVertexToGo.pop_front();
+
+		for (auto it_vertices = graph[currentlyOnVertex].cbegin();
+			it_vertices != graph[currentlyOnVertex].cend(); ++it_vertices)
+		{
+			if (!visited[*it_vertices])
+			{
+				visited[*it_vertices] = true;
+				queueVertexToGo.push_back(*it_vertices);
+			}
+		}
+	}
+
+	return path;
+}
+
+
+std::vector<int> GraphList::depthFirstSearch(int start)
+{
+	std::vector<bool> visited(vertices);
+	std::vector<int> path;
+
+	depthFirstSearchUtil(start, visited, path);
+	return path;
+}
+
+std::vector<int> GraphList::depthFirstSearchAllVertex()
+{
+	std::vector<bool> visited(vertices);
+	std::vector<int> path;
+
+	for (int i = 0; i < vertices; ++i)
+	{
+		if (!visited[i])
+			depthFirstSearchUtil(i, visited, path);
+	}
+	return path;
+}
+
+void GraphList::depthFirstSearchUtil(int currentVertextTraverse, std::vector<bool> &visited, std::vector<int> &path)
+{
+	visited[currentVertextTraverse] = true;
+	// actually we can do anything here for the vertex that already found, but for now just populate it into path array
+	path.push_back(currentVertextTraverse);
+
+	for (auto it_vertices = graph[currentVertextTraverse].cbegin();
+		it_vertices != graph[currentVertextTraverse].cend(); ++it_vertices)
+	{
+		if (!visited[*it_vertices])
+		{
+			visited[*it_vertices];
+			depthFirstSearchUtil(*it_vertices, visited, path);
+		}
+	}
+}
--- Graph/GraphList.h
+++ Graph/GraphList.h
@@ -2,6 +2,8 @@
 #include <list>
 #include <iostream>
 #include <iterator>
+#include <set>
+
 
 class GraphList
 {
@@ -9,111 +11,20 @@ public:
 	GraphList(int v) : vertices(v), graph(v), directed(false){};
 	GraphList(int v, bool direct) : vertices(v), graph(v), directed(direct) {};
 
-	void addEdge(int u, int v)
-	{
-		graph[u].push_back(v);
-		if (directed)
-			return;
-		graph[v].push_back(u);
-	}
-
-	void print()
-	{
-		for (int v = 0; v < graph.size(); ++v)
-		{
-			std::cout << std::endl << "Adjacency list of vertex "
-				<< v << std::endl << "head ";
-			for (auto x : graph[v])
-				std::cout << "->" << x;
-			std::cout << std::endl;
-		}
-	}
-
-	int getVertices() { return vertices; }
-
-	auto cbegin(int vertex) { return graph[vertex].cbegin(); }
-
-	auto cend(int vertex) { return graph[vertex].cend(); }
-
+	void addEdge(int u, int v);
+	void print();
+	void searchEdge(int src, int dest);
 	std::vector<int> breadthFirstSearch(int vectorStart);
 	std::vector<int> depthFirstSearch(int vectorStart);
 	std::vector<int> depthFirstSearchAllVertex();
+
+	int getVertices() { return vertices; }
+	auto cbegin(int vertex) { return graph[vertex].cbegin(); }
+	auto cend(int vertex) { return graph[vertex].cend(); }
 private:
 	void depthFirstSearchUtil(int vertex, std::vector<bool> &visited, std::vector<int> &path);
 
 	int vertices;
 	bool directed;
-	std::vector<std::vector<int>> graph;
+	std::vector<std::set<int>> graph;
 };
-
-
-std::vector<int> GraphList::breadthFirstSearch(int vectorStart)
-{
-	std::vector<bool> visited(vertices);
-
-	std::list<int> queueVertexToGo;
-	visited[vectorStart] = true;
-	
-	queueVertexToGo.push_front(vectorStart);
-
-	std::vector<int> path;
-	while (!queueVertexToGo.empty()) 
-	{
-		int currentlyOnVertex = queueVertexToGo.front();
-		// actually we can do anything here for the vertex that already found, but for now just populate it into path array
-		path.push_back(currentlyOnVertex);
-		queueVertexToGo.pop_front();
-
-		for (auto it_vertices = graph[currentlyOnVertex].cbegin(); 
-			it_vertices != graph[currentlyOnVertex].cend(); ++it_vertices)
-		{
-			if (!visited[*it_vertices])
-			{
-				visited[*it_vertices] = true;
-				queueVertexToGo.push_back(*it_vertices);
-			}
-		}
-	}
-
-	return path;
-}
-
-
-std::vector<int> GraphList::depthFirstSearch(int start)
-{
-	std::vector<bool> visited(vertices);
-	std::vector<int> path;
-
-	depthFirstSearchUtil(start, visited, path);
-	return path;
-}
-
-std::vector<int> GraphList::depthFirstSearchAllVertex()
-{
-	std::vector<bool> visited(vertices);
-	std::vector<int> path;
-
-	for (int i = 0; i < vertices; ++i)
-	{
-		if (!visited[i])
-			depthFirstSearchUtil(i, visited, path);
-	}
-	return path;
-}
-
-void GraphList::depthFirstSearchUtil(int currentVertextTraverse, std::vector<bool> &visited, std::vector<int> &path)
-{
-	visited[currentVertextTraverse] = true;
-	// actually we can do anything here for the vertex that already found, but for now just populate it into path array
-	path.push_back(currentVertextTraverse);
-
-	for (auto it_vertices = graph[currentVertextTraverse].cbegin(); 
-		it_vertices != graph[currentVertextTraverse].cend(); ++it_vertices)
-	{
-		if (!visited[*it_vertices])
-		{
-			visited[*it_vertices];
-			depthFirstSearchUtil(*it_vertices, visited, path);
-		}
-	}
-}
