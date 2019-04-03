aacbc108a73cd555f9d2d6107a5fa3fcfc0f19c0
Dicky Kamarullah <dicky.kamar14@gmail.com>
2019-04-03 08:31:07 +0700

    add one with only bitwise operator XoR and <<

--- Bit/CodingProblem.h
+++ Bit/CodingProblem.h
@@ -2,3 +2,17 @@ bool oppositeSign(int x, int y)
 {
 	return (x ^ y) < 0;
 }
+
+int addOne(int x)
+{
+	int flipper = 1;
+
+	while (x & flipper) // flip all the trailing 1 to 0
+	{
+		x = x ^ flipper;
+		flipper = flipper << 1; // shift the 1 to the right to cover all the one.
+	}
+
+	// if no more 1 to flip. lastly flip zero to 1
+	return x ^ flipper;
+}
