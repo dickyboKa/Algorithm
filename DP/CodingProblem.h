1f028cdede1f1dc16e447b186c65c8e8071603da
Dicky Kamarullah <dicky.kamar14@gmail.com>
2019-04-21 08:29:24 +0700

    number of arrangement with tabulation

--- DP/CodingProblem.h
+++ DP/CodingProblem.h
@@ -16,3 +16,20 @@ int numberArrangmentOf(int n)
 
 	return memoizationLookup[n] = numberArrangmentOf(n - 1) + numberArrangmentOf(n - 3) + numberArrangmentOf(n - 5);
 }
+
+int numberArragmentOfWithTabulation(int n)
+{
+	std::vector<int> result(n + 1);
+	result[0] = 1;
+
+	// build result bottom up
+	for (int i = 1; i <= n; ++i)
+	{
+		result[i] = result[i - 1];
+		if (i - 3 > -1)
+			result[i] += result[i - 3];
+		if (i - 5 > -1)
+			result[i] += result[i - 5];
+	}
+	return result[n];
+}
