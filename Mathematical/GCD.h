b7ace2975af93a9e831242aa059ce839dea301f2
Muhammad Kamarulah <muhammad.kamarulah@gameloft.com>
2019-04-23 09:46:19 +0700

    gcd of array of numbers

--- Mathematical/GCD.h
+++ Mathematical/GCD.h
@@ -1,10 +1,20 @@
 #pragma once
-
+#include <vector>
 
 int gcd(int a, int b)
 {
 	if (a == 0)
 		return b;
 	return gcd(b % a, a);
+}
+
+int gcd(std::vector<int> numbers)
+{
+	int result = numbers[0];
+	for (int i = 1; i < numbers.size(); ++i)
+	{
+		result = gcd(numbers[i], result);
+	}
+	return result;
 }
 
