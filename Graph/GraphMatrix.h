7dde42fd34067b5e4b89560e019a3b16dcde3b6c
Dicky Kamarullah <dicky.kamar14@gmail.com>
2019-05-12 08:12:46 +0700

    Minimum initial vertices to traverse whole matrix with given conditions

--- Graph/GraphMatrix.h
+++ Graph/GraphMatrix.h
@@ -1,10 +1,14 @@
 #pragma once
 #include <vector>
 #include <iostream>
+#include <algorithm>
 
 class GraphMatrix
 {
+	
 public:
+	enum DIRECTION { move_up, move_below, move_right, move_left };
+
 	GraphMatrix(int vertexSize) : graph(vertexSize), directed(false), V(vertexSize)
 	{
 		// creating 2d array
@@ -45,7 +49,104 @@ public:
 	}
 	int V;
 	auto operator[](int i) { return graph[i]; }
+	
+	void printMinimalSources();
+
 private:
+	void dfs(int i, int j, std::vector<std::vector<bool>> &visited);
+	std::vector< std::pair<int, int>> sortMatrixByWeightAndGetIndexPair();
+	bool moveValid(int i, int j, DIRECTION dir);
 	bool directed;
 	std::vector<std::vector<int>> graph;
 };
+
+
+void GraphMatrix::printMinimalSources()
+{
+	auto indexPairs = sortMatrixByWeightAndGetIndexPair();
+	std::vector<std::vector<bool>> visited(V, std::vector<bool>(V, false));
+
+	for (auto it = indexPairs.cbegin(); it != indexPairs.cend(); it++)
+	{
+		if (!visited[it->first][it->second])
+		{
+			std::cout << it->first << " " << it->second << std::endl;
+			dfs(it->first, it->second, visited);
+		}
+	}
+}
+
+std::vector< std::pair<int, int>> GraphMatrix::sortMatrixByWeightAndGetIndexPair()
+{
+	std::vector<std::pair<int, std::pair<int, int>>> adjacentPairs;
+
+	for (int i = 0; i < graph.size(); i++)
+	{
+		for (int j = i; j < graph[i].size(); ++j)
+		{
+			adjacentPairs.push_back(std::make_pair(graph[i][j], std::make_pair(i, j)));
+		}
+	}
+
+	std::sort(adjacentPairs.begin(), adjacentPairs.end(), std::greater< std::pair<int, std::pair<int, int>>>());
+
+	std::vector<std::pair<int, int>> results;
+	for (auto it = adjacentPairs.cbegin(); it != adjacentPairs.cend(); it++)
+	{
+		results.push_back(std::make_pair(it->second.first, it->second.second));
+	}
+
+	return results;
+}
+void GraphMatrix::dfs(int i, int j, std::vector<std::vector<bool>> &visited)
+{
+	visited[i][j] = true;
+
+	if (moveValid(i, j, GraphMatrix::DIRECTION::move_below) && !visited[i + 1][j])
+		dfs(i + 1, j, visited);
+
+
+	if (moveValid(i, j, GraphMatrix::DIRECTION::move_right) && !visited[i][j + 1])
+		dfs(i, j + 1, visited);
+
+	if (moveValid(i, j, GraphMatrix::DIRECTION::move_up) && !visited[i - 1][j])
+		dfs(i - 1, j, visited);
+
+	if (moveValid(i, j, GraphMatrix::DIRECTION::move_left) && !visited[i][j - 1])
+		dfs(i, j - 1, visited);
+}
+
+bool GraphMatrix::moveValid(int i, int j, DIRECTION dir)
+{
+	bool result = false;
+	switch (dir)
+	{
+	case GraphMatrix::move_up:
+		if (i - 1 >= 0 && graph[i][j] >= graph[i - 1][j])
+		{
+			result = true;
+		}
+		break;
+	case GraphMatrix::move_below:
+		if (i + 1 < V && graph[i][j] >= graph[i + 1][j])
+		{
+			result = true;
+		}
+		break;
+	case GraphMatrix::move_right:
+		if (j + 1 < V && graph[i][j] >= graph[i][j + 1])
+		{
+			result = true;
+		}
+		break;
+	case GraphMatrix::move_left:
+		if (j - 1 >= 0 && graph[i][j] >= graph[i][j - 1])
+		{
+			result = true;
+		}
+		break;
+	default:
+		break;
+	}
+	return result;
+}
